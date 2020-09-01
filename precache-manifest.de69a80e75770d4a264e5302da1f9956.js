3f877cb2f3050293cb1720781ef3aec82a28b9be
wizywigsolutions <wizywigsolutions@gmail.com>
2020-09-01 13:48:08 +0530

    Updates

---
+++ precache-manifest.de69a80e75770d4a264e5302da1f9956.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ec8233e52127cdebb432f3db2149ea5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "84e0c6b1d596d2f3cabc",
+    "url": "/monsters-rolodex/static/css/main.7c8a7ee5.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e0c6b1d596d2f3cabc",
+    "url": "/monsters-rolodex/static/js/main.fde6def1.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
